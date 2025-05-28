94222fe23689b9acef93a80b66760ddb3fc9ee89
Joel254010 <casalmarketingdeafiliados@gmail.com>
2025-05-28 11:44:32 -0300

    feat: adicionar emailResponsavel no schema Empresa

--- models/Empresa.js
+++ models/Empresa.js
@@ -1,13 +1,38 @@
 import mongoose from 'mongoose';
 
 const empresaSchema = new mongoose.Schema({
-  nome: String,
-  cidade: String,
-  estado: String,
-  segmento: String,
-  foto: String,
-  linkCartao: String,
+  nome: {
+    type: String,
+    required: true,
+  },
+  segmento: {
+    type: String,
+    required: true,
+  },
+  cidade: {
+    type: String,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  linkCartao: {
+    type: String,
+    required: true,
+  },
+  foto: {
+    type: String,
+    required: true,
+  },
+  emailResponsavel: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
 });
 
 const Empresa = mongoose.model('Empresa', empresaSchema);
+
 export default Empresa;
